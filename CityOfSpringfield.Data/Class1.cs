4069051ce0e26e18f4010722f67ec731389cc4f7
Derek Coppinger <Derek.Coppinger@DSNWorldWide.com>
2020-07-20 16:44:53 -0500

    initial commit

---
+++ CityOfSpringfield.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CityOfSpringfield.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ CityOfSpringfield/Models/Street.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityOfSpringfield.Models
+{
+    public class Street
+    {
+        public int id 
+    }
+}
