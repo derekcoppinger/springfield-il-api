156bf20c013f993e0e4de685c66275985854a59c
Derek Coppinger <Derek.Coppinger@DSNWorldWide.com>
2020-07-20 16:53:02 -0500

    add street dto

---
+++ CityOfSpringfield.Data/Dtos/StreetDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CityOfSpringfield.Data.Dtos
+{
+    class StreetDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Direction { get; set; }
+        public bool IsOneWay { get; set; }
+    }
+}
