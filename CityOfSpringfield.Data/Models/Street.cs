8e146f435f32d8e46db803bf2f2d28197341766d
Derek Coppinger <Derek.Coppinger@DSNWorldWide.com>
2020-07-28 13:04:00 -0500

    remove Id in street model

--- CityOfSpringfield.Data/Models/Street.cs
+++ CityOfSpringfield.Data/Models/Street.cs
@@ -7,9 +7,8 @@ namespace CityOfSpringfield.Models
 {
     public class Street
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Direction { get; set; }
-        
+
     }
 }
