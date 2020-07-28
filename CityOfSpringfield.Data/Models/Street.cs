d77ecf5efe88013488eaf44acb24c932f235c94e
Derek Coppinger <Derek.Coppinger@DSNWorldWide.com>
2020-07-28 15:53:01 -0500

    fix namespace

--- CityOfSpringfield.Data/Models/Street.cs
+++ CityOfSpringfield.Data/Models/Street.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CityOfSpringfield.Models
+namespace CityOfSpringfield.Data.Models
 {
     public class Street
     {
