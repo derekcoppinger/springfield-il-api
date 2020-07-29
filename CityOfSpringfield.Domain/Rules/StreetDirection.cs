8375939c0613abe0eb1e67e1eaa9b2b6bdb52121
Derek Coppinger <Derek.Coppinger@DSNWorldWide.com>
2020-07-29 10:06:23 -0500

    create and start domain project

---
+++ CityOfSpringfield.Domain/Rules/StreetDirection.cs
@@ -0,0 +1,10 @@
+namespace CityOfSpringfield.Domain.Rules
+{
+    public class StreetDirection
+    {
+        // Streets must always have a direction associated with them
+
+
+        
+    }
+}
---
+++ CityOfSpringfield.Domain/Rules/TransportationFair.cs
@@ -0,0 +1,10 @@
+namespace CityOfSpringfield.Domain.Rules
+{
+    public class TransportationFair
+    {
+        public decimal returnBusFair()
+        {
+            return 1.25;
+        }
+    }
+}
