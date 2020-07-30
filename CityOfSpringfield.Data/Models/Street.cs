c6424006ca9d0771ce17ebb719cfb1eb663d0fbb
Derek Coppinger <Derek.Coppinger@DSNWorldWide.com>
2020-07-30 18:27:25 -0500

    add primary id to street model

--- CityOfSpringfield.Data/Models/Street.cs
+++ CityOfSpringfield.Data/Models/Street.cs
@@ -7,6 +7,7 @@ namespace CityOfSpringfield.Data.Models
 {
     public class Street
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Direction { get; set; }
 
